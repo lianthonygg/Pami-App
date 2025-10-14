d0f5c97926428cb86af9560194f359007d0ac0f8
Li Anthony Dev <li.3108gomezgonzalez@gmail.com>
2025-10-13 22:58:57 -0400

    feat(project): update design drawer and added new views

--- lib/features/personas/presentation/views/personas_view.dart
+++ lib/features/personas/presentation/views/personas_view.dart
@@ -1,14 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:pami_app/core/widgets/dynamic_drawer.dart';
-import 'package:pami_app/features/personas/presentation/viewmodels/personas_viewmodel.dart';
+//import 'package:pami_app/features/personas/presentation/viewmodels/personas_viewmodel.dart';
 
 class PersonasView extends ConsumerWidget {
   const PersonasView({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final state = ref.watch(personasViewModelProvider);
+    //final state = ref.watch(personasViewModelProvider);
 
     return Scaffold(
       appBar: AppBar(title: Text("MINSAP App")),
--- lib/routing/router.dart
+++ lib/routing/router.dart
@@ -1,7 +1,10 @@
 import 'package:go_router/go_router.dart';
 import 'package:pami_app/features/auth/presentation/views/login_view.dart';
+import 'package:pami_app/features/gestograma/presentation/views/gestograma_view.dart';
 import 'package:pami_app/features/home/presentation/views/home_view.dart';
 import 'package:pami_app/features/personas/presentation/views/personas_view.dart';
+import 'package:pami_app/features/posgestograma/presentation/views/posgestograma_view.dart';
+import 'package:pami_app/features/pregestograma/presentation/views/pregestograma_view.dart';
 import 'package:pami_app/routing/routes.dart';
 
 GoRouter router() => GoRouter(
@@ -23,6 +26,24 @@ GoRouter router() => GoRouter(
       path: Routes.login,
       builder: (context, state) {
         return LoginView();
+      },
+    ),
+    GoRoute(
+      path: Routes.pregestograma,
+      builder: (context, state) {
+        return PregestogramaView();
+      },
+    ),
+    GoRoute(
+      path: Routes.gestograma,
+      builder: (context, state) {
+        return GestogramaView();
+      },
+    ),
+    GoRoute(
+      path: Routes.posgestograma,
+      builder: (context, state) {
+        return PosgestogramaView();
       },
     ),
   ],
