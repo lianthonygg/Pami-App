c7caa294fa5f39f8d601c234cca60729f4566cde
Li Anthony <li.3108gomezgonzalez@gmail.com>
2026-03-23 22:36:08 -0400

    feat: add createGestante and createGestanteServer methods to GestanteRepository

--- lib/features/gestograma/domain/repositories/gestante_repository.dart
+++ lib/features/gestograma/domain/repositories/gestante_repository.dart
@@ -1,7 +1,13 @@
+import 'package:pami_app/features/gestograma/data/model/create_gestante_model.dart';
 import 'package:pami_app/features/gestograma/data/model/gestante_model.dart';
 import 'package:pami_app/features/gestograma/domain/entities/gestante.dart';
+import 'package:pami_app/features/personas/data/model/create_persona_model.dart';
 
 abstract class GestanteRepository {
+  Future<void> createGestante(CreateGestanteRequest request);
+
+  Future<ResponseCreatePersonaModel> createGestanteServer(CreateGestanteRequest request);
+
   Stream<List<GestantesResponseModel>> watchGestantes();
 
   Future<PersonaWithGestanteDetail?> getGestanteByCi(String ci);
